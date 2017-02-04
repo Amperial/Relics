961758d13cf7e2e33909ae40549d8efa193d7d2b
ampayne2 <payneaustin5@gmail.com>
2017-02-04 00:05:38 -0500

    Remove LGPL License, this is proprietary code
    Signed-off-by: ampayne2 <payneaustin5@gmail.com>

--- modules/api/src/main/java/ninja/amp/items/api/item/attribute/attributes/sockets/Gem.java
+++ modules/api/src/main/java/ninja/amp/items/api/item/attribute/attributes/sockets/Gem.java
@@ -1,20 +1,12 @@
 /*
  * This file is part of AmpItems API.
  *
- * Copyright (c) 2017 <http://github.com/ampayne2/AmpItems//>
+ * Copyright (c) 2017, Austin Payne <payneaustin5@gmail.com - http://github.com/ampayne2>
  *
- * AmpItems API is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * All Rights Reserved.
  *
- * AmpItems API is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with AmpItems API.  If not, see <http://www.gnu.org/licenses/>.
+ * Unauthorized copying and/or distribution of AmpItems API,
+ * via any medium is strictly prohibited.
  */
 package ninja.amp.items.api.item.attribute.attributes.sockets;
 
--- modules/nmshandler/src/main/java/ninja/amp/items/nms/nbt/NBTTagList.java
+++ modules/nmshandler/src/main/java/ninja/amp/items/nms/nbt/NBTTagList.java
@@ -1,20 +1,12 @@
 /*
  * This file is part of AmpItems NMS API.
  *
- * Copyright (c) 2017 <http://github.com/ampayne2/AmpItems//>
+ * Copyright (c) 2017, Austin Payne <payneaustin5@gmail.com - http://github.com/ampayne2>
  *
- * AmpItems NMS API is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * All Rights Reserved.
  *
- * AmpItems NMS API is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with AmpItems NMS API.  If not, see <http://www.gnu.org/licenses/>.
+ * Unauthorized copying and/or distribution of AmpItems NMS API,
+ * via any medium is strictly prohibited.
  */
 package ninja.amp.items.nms.nbt;
 
