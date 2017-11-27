6f95e407364b98f0df43c6a068ebdfb184e4ea51
ampayne2 <payneaustin5@gmail.com>
2017-11-26 21:55:22 -0500

    Finish identifiable item attribute
    Signed-off-by: ampayne2 <payneaustin5@gmail.com>

--- modules/plugin/src/main/java/com/herocraftonline/items/item/attributes/IdentifiableAttribute.java
+++ modules/plugin/src/main/java/com/herocraftonline/items/item/attributes/IdentifiableAttribute.java
@@ -11,18 +11,25 @@
 package com.herocraftonline.items.item.attributes;
 
 import com.herocraftonline.items.api.ItemPlugin;
+import com.herocraftonline.items.api.item.Clickable;
 import com.herocraftonline.items.api.item.Item;
 import com.herocraftonline.items.api.item.attribute.attributes.Identifiable;
 import com.herocraftonline.items.api.item.attribute.attributes.base.BaseAttribute;
 import com.herocraftonline.items.api.item.attribute.attributes.base.BaseAttributeFactory;
 import com.herocraftonline.items.api.storage.nbt.NBTTagCompound;
 import com.herocraftonline.items.item.DefaultAttribute;
+import com.herocraftonline.items.nms.NMSHandler;
 import com.herocraftonline.items.util.EncryptUtil;
 import com.herocraftonline.items.util.ItemUtil;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
-public class IdentifiableAttribute extends BaseAttribute<Identifiable> implements Identifiable {
+import java.util.Optional;
+
+public class IdentifiableAttribute extends BaseAttribute<Identifiable> implements Identifiable, Clickable {
 
     private final String encryptedItem;
 
@@ -41,6 +48,21 @@ public class IdentifiableAttribute extends BaseAttribute<Identifiable> implement
     public void saveToNBT(NBTTagCompound compound) {
         super.saveToNBT(compound);
         compound.setString("item", encryptedItem);
+    }
+
+    @Override
+    public void onClick(PlayerInteractEvent event, Item item) {
+        if (event.getAction() == Action.LEFT_CLICK_AIR) {
+            // Replace item with identified item
+            ItemStack identifiable = event.getItem();
+            NBTTagCompound identified = NMSHandler.instance().toNBT(identifyItem());
+            Optional<ItemStack> updated = NMSHandler.instance().replaceNBT(identifiable, identified);
+
+            // Update item in player inventory
+            Player player = event.getPlayer();
+            updated.ifPresent(updatedItem -> player.getEquipment().setItemInMainHand(updatedItem));
+            player.updateInventory();
+        }
     }
 
     public static class Factory extends BaseAttributeFactory<Identifiable> {
